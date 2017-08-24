5a823a9da87243f0ffc173a76273d719c58856f3
Diogo Cardoso <diogocardoso92@outlook.com>
2017-08-24 21:16:36 +0100

    Fixed mongoose config file
    Node was throwing a deprecation error related to mongoose. This is a known bug (https://github.com/Automattic/mongoose/issues/5399) so I used a work-around to avoid it.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const dburl = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dburl);
+mongoose.connect(dburl, {
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE,
+    useMongoClient: true
+});
 
 module.exports = {mongoose};

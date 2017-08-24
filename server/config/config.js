6c7d2c5da5322417e5e12447a979551ba0cfdc47
Diogo Cardoso <diogocardoso92@outlook.com>
2017-08-24 21:18:36 +0100

    use const instead of let
    There is no need to use 'let' here since the variables are constants.

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
-let env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
     const config = require('./config.json');
-    let envConfig = config[env];
+    const envConfig = config[env];
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key];
     });
